132c821a9b053b53adf79f6019553174a1a92adf
thomas-jacque-schultz <schultz.thomas@free.fr>
2019-06-13 11:09:27 +0200

    map v2

--- TrapClap/app/src/main/java/pack/clap/Map2DActivity.java
+++ TrapClap/app/src/main/java/pack/clap/Map2DActivity.java
@@ -43,45 +43,45 @@ public class Map2DActivity extends AppCompatActivity {
         link = new HashMap();
 
         link.put(1,new Point(74,15));
-        link.put(2,new Point(33,47));
-        link.put(3,new Point(32,43));
-        link.put(4,new Point(24,60));
-        link.put(5,new Point(24,80));
-        link.put(6,new Point(24,116));
-        link.put(7,new Point(24,140));
-        link.put(8,new Point(24,155));
-        link.put(9,new Point(24,198));
-        link.put(10,new Point(24,244));
-        link.put(11,new Point(24,277));
-        link.put(12,new Point(70,277));
-        link.put(13,new Point(125,277));
-        link.put(14,new Point(145,277));
-        link.put(15,new Point(171,277));
-        link.put(16,new Point(208,277));
-        link.put(17,new Point(263,277));
-        link.put(18,new Point(285,277));
-        link.put(19,new Point(307,277));
-        link.put(20,new Point(350,277));
-        link.put(21,new Point(24,294));
-        link.put(22,new Point(24,352));
-        link.put(23,new Point(24,414));
-        link.put(24,new Point(24,457));
-        link.put(25,new Point(47,457));
-        link.put(26,new Point(24,477));
-        link.put(27,new Point(24,531));
-        link.put(28,new Point(24,547));
-        link.put(29,new Point(24,613));
-        link.put(30,new Point(72,531));
-        link.put(31,new Point(67,483));
-        link.put(32,new Point(103,531));
-        link.put(33,new Point(128,531));
-        link.put(34,new Point(172,531));
-        link.put(35,new Point(199,531));
-        link.put(36,new Point(218,531));
-        link.put(37,new Point(257,531));
-        link.put(38,new Point(274,531));
-        link.put(39,new Point(298,531));
-        link.put(40,new Point(355,531));
+        link.put(2,new Point(24,50));
+        link.put(3,new Point(24,62));
+        link.put(4,new Point(24,92));
+        link.put(5,new Point(24,125));
+        link.put(6,new Point(24,138));
+        link.put(7,new Point(24,193));
+        link.put(8,new Point(24,205));
+        link.put(9,new Point(24,248));
+        link.put(10,new Point(24,292));
+        link.put(11,new Point(24,307));
+        link.put(12,new Point(70,307));
+        link.put(13,new Point(125,307));
+        link.put(14,new Point(145,307));
+        link.put(15,new Point(171,307));
+        link.put(16,new Point(208,307));
+        link.put(17,new Point(263,307));
+        link.put(18,new Point(285,307));
+        link.put(19,new Point(307,307));
+        link.put(20,new Point(350,307));
+        link.put(21,new Point(24,322));
+        link.put(22,new Point(24,402));
+        link.put(23,new Point(24,470));
+        link.put(24,new Point(24,495));
+        link.put(25,new Point(47,495));
+        link.put(26,new Point(24,527));
+        link.put(27,new Point(24,575));
+        link.put(28,new Point(24,595));
+        link.put(29,new Point(24,662));
+        link.put(30,new Point(72,581));
+        link.put(31,new Point(67,531));
+        link.put(32,new Point(103,581));
+        link.put(33,new Point(133,581));
+        link.put(34,new Point(165,581));
+        link.put(35,new Point(202,581));
+        link.put(36,new Point(224,581));
+        link.put(37,new Point(264,581));
+        link.put(38,new Point(284,581));
+        link.put(39,new Point(305,581));
+        link.put(40,new Point(345,581));
 
 
 
@@ -143,7 +143,7 @@ public class Map2DActivity extends AppCompatActivity {
         //textView.setText(map.getPathFromTo(11,global.getRoom()).toString());
 
 
-        this.maj((map.getPathFromTo(11,global.getRoom())));
+        this.maj((map.getPathFromTo(1,global.getRoom())));
 
     }
 
@@ -151,7 +151,7 @@ public class Map2DActivity extends AppCompatActivity {
         ImageView im= (ImageView) findViewById( R.id.Map2D);
         Paint mPaint = new Paint();
         mPaint.setDither(true);
-        mPaint.setColor(getResources().getColor(R.color.color_Blue));
+        mPaint.setColor(getResources().getColor(R.color.color_LightBlue));
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
@@ -160,22 +160,34 @@ public class Map2DActivity extends AppCompatActivity {
         Point mPoint = new Point();
 
 
-        Bitmap bm=  BitmapFactory.decodeResource(getResources(),R.drawable.map_noir);
+        Bitmap bm=  BitmapFactory.decodeResource(getResources(),R.drawable.map_finale );
         bm=bm.copy(bm.getConfig(),true);
         Canvas c=new Canvas(bm);
         im.draw(c);
         Iterator i=m.iterator();
         Graph.Vertex n;
-        Graph.Vertex t;
+        Graph.Vertex t=null;
+       /* for (int j=2;j<12;j++){
+            c.drawLine(200,200,((Point)this.link.get(j)).x,((Point)this.link.get(j)).y,mPaint);
+        }*/
+
         n=(Graph.Vertex)i.next();
+        float x=((Point)this.link.get(Integer.parseInt(n.getId()))).x;
+        float y=((Point)this.link.get(Integer.parseInt(n.getId()))).y;
+        c.drawOval(x-3,y+3,x+3,y-3,mPaint);
         while(i.hasNext()){
             t=(Graph.Vertex)i.next();
-            System.out.println("||"+Integer.parseInt(n.getId()));
-            System.out.println(t.getId() + "||");
+            //System.out.println("||"+Integer.parseInt(n.getId()));
+            //System.out.println(t.getId() + "||");
 
             c.drawLine(((Point)this.link.get(Integer.parseInt(n.getId()))).x,((Point)this.link.get(Integer.parseInt(n.getId()))).y,((Point)this.link.get(Integer.parseInt(t.getId()))).x,((Point)this.link.get(Integer.parseInt(t.getId()))).y,mPaint);
             n=t.clone();
+
         }
+
+        x=((Point)this.link.get(Integer.parseInt(t.getId()))).x;
+        y=((Point)this.link.get(Integer.parseInt(t.getId()))).y;
+        c.drawOval(x,y+5,x+10,y-5,mPaint);
 
         //bm.setPixel(10,10, Color.red(1));
 
